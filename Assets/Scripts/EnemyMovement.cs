0a8577c351b39eb58fc0184366bf539ced4c8717
Luckynotam <l.w.t.maton@gmail.com>
2026-04-10 10:38:43 +0200

    Finished the Enemy Movment Script

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,12 +5,14 @@ public class EnemyMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private float speed = 3f;
+    [SerializeField] private float speed = 1.75f;
     [SerializeField] private int startDirection = 1;
 
     private int currentDirection;
 
-    private float halfWidth; 
+    private float halfWidth;
+
+    private Vector2 movement; 
 
 
 
@@ -19,13 +21,38 @@ public class EnemyMovement : MonoBehaviour
     {
         halfWidth = spriteRenderer.bounds.extents.x;
         currentDirection = startDirection;
+        spriteRenderer.flipX =  startDirection == 1 ? false : true;
+
 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rigidBody.linearVelocity = Vector2.right * speed * currentDirection;
+        movement.x = speed * currentDirection;
+        movement.y = rigidBody.linearVelocityY;
+        rigidBody.linearVelocity = movement;
+        
+
 
     }
+
+    private void SetDirection()
+    {
+        if (Physics2D.Raycast(transform.position, Vector2.right, halfWidth + 0.1f, LayerMask.GetMask("Ground")) && 
+            rigidBody.linearVelocity.x > 0)
+        {
+            currentDirection *= -1;
+            spriteRenderer.flipX = true; 
+
+        }
+        else if (Physics2D.Raycast(transform.position, Vector2.left, halfWidth + 0.1f, LayerMask.GetMask("Ground")) && 
+            rigidBody.linearVelocityX < 0)
+        {
+            currentDirection *= -1;
+        }
+            Debug.DrawRay(transform.position, Vector2.right * (halfWidth * 0.1f), Color.red);
+            Debug.DrawRay(transform.position, Vector2.left * (halfWidth * 0.1f), Color.red);
+
+    }   
 }
